0cbc696ff8a4d45044b32f1c929e656ee02f8b41
David Wybourn <david.wybourn@gmail.com>
2019-03-18 14:15:28 +0000

    HTBHF-632 Added error handler configuration.

---
+++ error_handler/README.md
@@ -0,0 +1,11 @@
+# Request Context Module
+
+This sub-module contains all the code that is needed to return formatted error responses. 
+
+Adding `RequestContextConfiguration` to your application context will provide you with the following beans:
+
+- `headerInterceptor`: Interceptor added to the provided `RestTemplate`
+- `mdcWrapper`: A component wrapper around MDC
+- `requestContext`: A `RequestContext` bean scoped to the request storing request ID and session ID 
+- `requestIdFilter`: A filter used on incoming requests to add the provided request and session ID to the MDC for logging
+- `restTemplateWithIdHeaders`: A `RestTemplate` that adds session ID and request ID headers to each request
---
+++ error_handler/src/main/java/uk/gov/dhsc/htbhf/errorhandler/ErrorHandlerConfiguration.java
@@ -0,0 +1,26 @@
+package uk.gov.dhsc.htbhf.errorhandler;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import uk.gov.dhsc.htbhf.requestcontext.RequestContext;
+import uk.gov.dhsc.htbhf.requestcontext.RequestContextConfiguration;
+
+@Configuration
+@Import(RequestContextConfiguration.class)
+@ConditionalOnMissingBean(annotation = ControllerAdvice.class)
+public class ErrorHandlerConfiguration {
+
+    private final RequestContext requestContext;
+
+    public ErrorHandlerConfiguration(RequestContext requestContext) {
+        this.requestContext = requestContext;
+    }
+
+    @Bean
+    public ErrorHandler errorHandler() {
+        return new ErrorHandler(requestContext);
+    }
+}
