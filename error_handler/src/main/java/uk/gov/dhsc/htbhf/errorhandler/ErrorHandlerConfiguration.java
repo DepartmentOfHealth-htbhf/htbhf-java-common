502e95dec56a87a904a6dfc73b94f636047bbe76
David Wybourn <david.wybourn@gmail.com>
2019-03-18 15:18:45 +0000

    HTBHF-632 Removed spring boot dependency.

--- error_handler/src/main/java/uk/gov/dhsc/htbhf/errorhandler/ErrorHandlerConfiguration.java
+++ error_handler/src/main/java/uk/gov/dhsc/htbhf/errorhandler/ErrorHandlerConfiguration.java
@@ -1,16 +1,13 @@
 package uk.gov.dhsc.htbhf.errorhandler;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import uk.gov.dhsc.htbhf.requestcontext.RequestContext;
 import uk.gov.dhsc.htbhf.requestcontext.RequestContextConfiguration;
 
 @Configuration
 @Import(RequestContextConfiguration.class)
-@ConditionalOnMissingBean(annotation = ControllerAdvice.class)
 public class ErrorHandlerConfiguration {
 
     private final RequestContext requestContext;
