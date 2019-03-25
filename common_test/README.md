75447a0338c8ea0897c37d1f22100f6b35885ba4
Mike Smith <mike.smith@sescon.co.uk>
2019-03-25 12:24:40 +0000

    HTBHF-634 Add common test module for swagger and constraint violation assertion classes to be reused.

---
+++ common_test/README.md
@@ -0,0 +1,7 @@
+# Common Test Module
+
+This sub-module contains some common code which can be used for projects, it contains:
+
+ - `SwaggerGenerationUtil` - contains the common code required to load the Swagger from an application and store in a file.
+ - `ConstraintViolationAssert` - contains custom AssertJ assertions for use with unit testing validation.
+ 
---
+++ common_test/src/main/java/uk/gov/dhsc/htbhf/swagger/SwaggerGenerationUtil.java
@@ -0,0 +1,45 @@
+package uk.gov.dhsc.htbhf.swagger;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.google.common.io.Files;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Can be used to check that the Swagger spec is available to the running application
+ * on the given port ans then saves it in the root project directory as swagger.yml.
+ */
+public class SwaggerGenerationUtil {
+
+    public static void swaggerDocumentationRetrieved(TestRestTemplate testRestTemplate, int applicationPort) throws IOException {
+        String response = testRestTemplate.getForObject(buildUrl(applicationPort), String.class);
+
+        String jsonAsYaml = convertJsonToYaml(response);
+
+        assertThat(jsonAsYaml).isNotBlank();
+        assertThat(jsonAsYaml).containsSequence("swagger: \"2.0\"");
+
+        File swaggerFile = new File("../swagger.yml");
+        Files.asCharSink(swaggerFile, Charset.defaultCharset()).write(jsonAsYaml);
+    }
+
+    private static String convertJsonToYaml(String response) throws IOException {
+        JsonNode jsonNodeTree = new ObjectMapper().readTree(response);
+        return new YAMLMapper()
+                .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
+                .writeValueAsString(jsonNodeTree);
+    }
+
+    private static String buildUrl(int applicationPort) {
+        return "http://localhost:" + applicationPort + "/v2/api-docs";
+    }
+}
+
