78d07cba2bbbd296cabcf4468f4ac6e598292646
Matt Smith <msmith@equalexperts.com>
2019-10-01 09:27:49 +0100

    HTBHF-2342 Updated the restTemplate to use an ObjectMapper configured for dates as strings

--- common_rest/src/main/java/uk/gov/dhsc/htbhf/requestcontext/RequestContextConfiguration.java
+++ common_rest/src/main/java/uk/gov/dhsc/htbhf/requestcontext/RequestContextConfiguration.java
@@ -1,9 +1,24 @@
 package uk.gov.dhsc.htbhf.requestcontext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.dhsc.htbhf.requestcontext.aop.RequestContextAdvisor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Configuration class to be used to setup the required beans to setup the request scoped
@@ -29,10 +44,40 @@ public class RequestContextConfiguration {
 
     @Bean(name = "restTemplateWithIdHeaders")
     public RestTemplate restTemplateWithIdHeaders() {
-        var restTemplate = new RestTemplate();
-        var interceptors = restTemplate.getInterceptors();
-        interceptors.add(headerInterceptor());
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(replaceMappingJackson2HttpMessageConverter(restTemplate.getMessageConverters()));
+        restTemplate.getInterceptors().add(headerInterceptor());
         return restTemplate;
+    }
+
+    private List<HttpMessageConverter<?>> replaceMappingJackson2HttpMessageConverter(List<HttpMessageConverter<?>> originalConverters) {
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        converters.add(mappingJackson2HttpMessageConverter());
+        converters.addAll(originalConverters.stream()
+                .filter(converter -> !(converter instanceof MappingJackson2HttpMessageConverter))
+                .collect(Collectors.toList())
+        );
+        return converters;
+    }
+
+    private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper());
+        return converter;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ISO_LOCAL_DATE));
+
+        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+        objectMapper.registerModule(javaTimeModule);
+
+        return objectMapper;
     }
 
     @Bean
